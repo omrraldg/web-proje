27f9badefee91e892928eb3b34778bb1324d5dfb
omrraldg <farukalada@gmail.com>
2025-05-14 23:36:37 +0300

    Şehir sayfasının içeriği eklendi, yeni özellikler eklendi

---
+++ scritpt.cs
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let carouselElement = document.querySelector("#sehrimSlider");
+    let carousel = new bootstrap.Carousel(carouselElement, {
+        interval: 3000, // 3 saniyede bir geçiş
+        ride: "carousel"
+    });
+});
+
+document.querySelectorAll(".nav-link").forEach(link => {
+    link.addEventListener("click", function () {
+        document.querySelectorAll(".nav-link").forEach(nav => nav.classList.remove("active"));
+        this.classList.add("active");
+    });
+});
